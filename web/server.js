8ecbeb01f26f84fc64b6cbe19789ff1b90055c39
刘纪平 <jipings@outlook.com>
2017-11-02 11:17:37 +0800

    增加对文件访问错误的处理

--- web/server.js
+++ web/server.js
@@ -9,8 +9,27 @@ var root = __dirname;
 var server = http.createServer((req, res) => {
     var url = parse(req.url);
     var path = join(root, url.pathname); // 构造绝对路径
-    var stream = fs.createReadStream(path); // 创建fs.ReadStream
-    stream.pipe(res); // res.end() 会在steam.pipe()内部调用
+    
+    fs.stat(path, (err, stat) => {
+        if(err) {
+            if('ENOENT' == err.code) {
+                res.statusCode = 404;
+                res.end();
+            } else {
+                res.statusCode = 500;
+                res.end('Internal Server Error');
+            }
+        } else {
+            res.setHeader('Content-Length', stat.size);
+            var stream = fs.createReadStream(path); // 创建fs.ReadStream
+            stream.pipe(res); // res.end() 会在steam.pipe()内部调用
+            stream.on('error', (err) => {
+                res.statusCode = 500;
+                res.end('Internal Server Error');
+            });
+        }
+    })
+    
 });
 
 server.listen(3000, () => {console.log('🌍 server listening on 3000 port')});
