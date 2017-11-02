a10cfd88848fe64fb71f0b1a7196875faed44165
刘纪平 <jipings@outlook.com>
2017-11-02 10:50:12 +0800

    add pipe

--- web/server.js
+++ web/server.js
@@ -10,13 +10,7 @@ var server = http.createServer((req, res) => {
     var url = parse(req.url);
     var path = join(root, url.pathname); // 构造绝对路径
     var stream = fs.createReadStream(path); // 创建fs.ReadStream
-    stream.on('data', (chunk) => { // 将文件写入响应中
-        res.write(chunk);
-    });
-
-    stream.on('end', () => { // 文件写完后结束响应
-        res.end();
-    })
+    stream.pipe(res); // res.end() 会在steam.pipe()内部调用
 });
 
 server.listen(3000, () => {console.log('🌍 server listening on 3000 port')});
