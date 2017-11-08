833abc1e9f83f7fb366b86ee5c7b3f421f948739
刘纪平 <jipings@outlook.com>
2017-11-08 18:09:14 +0800

    add connect

---
+++ Connect/6-1.js
@@ -0,0 +1,51 @@
+
+var connect = require('connect');
+var app = connect();
+
+app.listen(3000);
+
+app.use(logger)
+    .use('/admin', restrict)
+    .use('/admin', admin)
+    .use(hello);
+
+function logger(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+};
+
+function hello(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+}
+
+function restrict(req, res, next) {
+    console.log(req.headers)
+    var authorization = req.headers.authorization;
+    if(!authorization) {
+        return next(new Error('Unauthorized'));
+    }
+    console.log(authorization, 'authorization');
+    var parts = authorization.split(' ');
+    var scheme = parts[0];
+    var auth = new Buffer(parts[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+
+    authenticateWithDatabase(user, pass, function(err) {
+        if(err) return next(err);
+        next(); 
+    });
+}
+
+function admin(req, res, next) {
+    switch (req.url) {
+        case '/':
+            res.end('try /users');
+            break;
+        case '/users':
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(['tobi', 'loki', 'jane']));
+            break;
+    }
+}
---
+++ Connect/connect.md
@@ -0,0 +1,4 @@
+
+## authorization
+
+    curl --user tobi:ferret http://localhost:3000/admin/users
