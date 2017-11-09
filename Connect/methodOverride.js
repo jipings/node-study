22920c3e800d95097c75e012340833da239b90ec
刘纪平 <jipings@outlook.com>
2017-11-10 00:22:12 +0800

    伪造http方法

--- Connect/methodOverride.js
+++ Connect/methodOverride.js
@@ -1,4 +1,28 @@
 
 var connect = require('connect');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var morgan = require('morgan');
 
-connect.use()
+var app = connect();
+app.listen(3000);
+
+function edit(req, res, next) {
+    if('GET' != req.method) return next();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<form method="post" type="application/x-www-form-urlencoded"><input type="hidden" name="_method" value="put" /><input type="text" name="user" value="Tobi" /><input type="submit" value="Update" /></form>');
+    res.end();
+};
+
+function update(req, res, next) {
+    console.log(req.method, req.body);
+    if('POST' != req.method) return next();
+    
+    res.end(`Updated name to ${req.body.user}`);
+};
+
+
+app.use(morgan('dev'))
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(edit)
+    .use(update);
